faa6f35ac8e7321ce072f4369176e61d55224dda
Evgenia Patsoni <devops-d2.eurodyn.com>
2022-03-09 11:25:44 +0200

    feat: added new DummyCart object

--- src/main/java/DummyCart.java
+++ src/main/java/DummyCart.java
@@ -9,6 +9,7 @@ public class DummyCart {
   public static void main(String[] args) {
     DummyCart myObj1 = new DummyCart();  // Object 1
     DummyCart myObj2 = new DummyCart();  // Object 2
+    DummyCart myObj3 = new DummyCart();  // Object 2
 
     System.out.println(myObj1.x);
     System.out.println(myObj2.x);
