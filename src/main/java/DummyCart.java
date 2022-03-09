0e0016e651b8bc56d8db8ecd2137e500b12c11cd
Evgenia Patsoni <devops-d2.eurodyn.com>
2022-03-09 12:58:56 +0200

    feat: new class

--- src/main/java/DummyCart.java
+++ src/main/java/DummyCart.java
@@ -5,11 +5,12 @@ import org.apache.log4j.Logger;
 public class DummyCart {
   int x = 5;
   int a=5;
+  int c=6;
 
   public static void main(String[] args) {
     DummyCart myObj1 = new DummyCart();  // Object 1
     DummyCart myObj2 = new DummyCart();  // Object 2
-    //test
+    DummyCart myObj3 = new DummyCart();  // Object 2
 
     System.out.println(myObj1.x);
     System.out.println(myObj2.x);
---
+++ src/main/java/HappyCart.java
@@ -0,0 +1,10 @@
+public class HappyCart {
+
+    private String name;
+    private int age;
+
+    public HappyCart(String name, int age){
+        this.age=age;
+        this.name=name;
+    }
+}
