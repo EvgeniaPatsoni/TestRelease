4ccd65f52d1b1965cbebfb28b51aa871475c05d6
Evgenia Patsoni <devops-d2.eurodyn.com>
2022-03-16 15:28:30 +0200

    fix: test

--- src/main/java/HappyCart.java
+++ src/main/java/HappyCart.java
@@ -3,6 +3,7 @@ public class HappyCart {
     private String name;
     private int age;
     private int hello;
+    private int cart;
 
     public HappyCart(String name, int age){
         this.age=age;
