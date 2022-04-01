0923194cae7f46b7a26ae967b85185a2a45d3608
Evgenia Patsoni <devops-d2.eurodyn.com>
2022-04-01 14:38:32 +0300

    modified files

--- CHANGE.md
+++ CHANGE.md
@@ -1,9 +1,2 @@
-## Changelog
-0.0.3
+0.0.4
 ## Commits
-- release: prepare release 0.0.3 (7642f10)
-- feat: new functionality added (15f1478)
-- fix: fixes in ... class (ecb579a)
-- refactor: code refactoring in ... class (c32e99a)
-- updated changelog (2526d8c)
-- release: prepare 0.0.3-SNAPSHOT (e40b0f6)
--- change.sh
+++ change.sh
@@ -8,4 +8,33 @@ echo '## Changelog' >> $OUTPUT_FILE
 git tag --sort=taggerdate | tail -1 >> $OUTPUT_FILE
 echo '## Commits' >> $OUTPUT_FILE 
 # generate the changelog
-git log --pretty="- %s (%h)" $(git tag --sort=-creatordate | head -2)...$(git tag --sort=-creatordate | head -1) >> $OUTPUT_FILE
+#GIT_LOG=`git log --pretty="- %s (%h)" $(git tag --sort=-taggerdate | head -2)...$(git tag --sort=-taggerdate | head -1)`
+GIT_LOG=`git log --pretty="*%s" $(git tag --sort=-taggerdate | head -2)...$(git tag --sort=-taggerdate | head -1)`
+echo $GIT_LOG
+
+features=()
+fixes=()
+releases=()
+
+##Split commits uppon * character and save to arrays depending on commit message##
+IFS=* commits=($GIT_LOG)
+for i in "${commits[@]}"; do
+  if [[ $i = feat:* ]]
+  then
+    features+=($i)
+  elif [[ $i = fix:* ]] 
+  then
+    fixes+=($i)
+  elif [[ $i = release:* ]]  
+  then
+    releases+=($i)
+  fi  
+done
+
+printf "%s\n" "${features[@]}"
+printf "%s\n" "${fixes[@]}"
+printf "%s\n" "${releases[@]}"
+
+
+#[[ $GIT_LOG = feat:* ]] && echo '### Features' >> $OUTPUT_FILE 
+#git log --pretty="- %s (%h)" $(git tag --sort=-taggerdate | head -2)...$(git tag --sort=-taggerdate | head -1) >> $OUTPUT_FILE
