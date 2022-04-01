afaf666dc972609c886f8627e1b43daea8d3d38e
Evgenia Patsoni <devops-d2.eurodyn.com>
2022-04-01 15:17:58 +0300

    release: prepare release 0.0.6

--- CHANGE.md
+++ CHANGE.md
@@ -1,2 +1,16 @@
+## Changelog
 0.0.4
 ## Commits
+## Changelog
+0.0.5
+### Features
+feat: test feature 2 in file.... (c49223e)
+ feat: test feature 1 in file.... (6b99528)
+
+### Fixes
+fix: test fix number-3 (9dd13bd)
+ fix: test fix number-2 (1c5c86e)
+ fix: test fix number-1 (5ce9e30)
+### Releases
+release: prepare release 0.0.5 (db123e2)
+
--- change.sh
+++ change.sh
@@ -4,19 +4,21 @@ set -eu
 # destination of the final changelog file
 OUTPUT_FILE=CHANGE.md
 
+# print Changelog in the output file
 echo '## Changelog' >> $OUTPUT_FILE
+# find and print the current tag version in the output file
 git tag --sort=taggerdate | tail -1 >> $OUTPUT_FILE
-echo '## Commits' >> $OUTPUT_FILE 
+
 # generate the changelog
-#GIT_LOG=`git log --pretty="- %s (%h)" $(git tag --sort=-taggerdate | head -2)...$(git tag --sort=-taggerdate | head -1)`
-GIT_LOG=`git log --pretty="*%s" $(git tag --sort=-taggerdate | head -2)...$(git tag --sort=-taggerdate | head -1)`
+GIT_LOG=`git log --pretty="*%s (%h)" $(git tag --sort=-taggerdate | head -2)...$(git tag --sort=-taggerdate | head -1)`
 echo $GIT_LOG
 
 features=()
 fixes=()
 releases=()
+deletions=()
 
-##Split commits uppon * character and save to arrays depending on commit message##
+# Split commits uppon * character and save to arrays depending on commit message#
 IFS=* commits=($GIT_LOG)
 for i in "${commits[@]}"; do
   if [[ $i = feat:* ]]
@@ -28,13 +30,36 @@ for i in "${commits[@]}"; do
   elif [[ $i = release:* ]]  
   then
     releases+=($i)
+  elif [[ $i = delete:* ]]   
+  then
+    deletions+=($i) 
   fi  
 done
 
-printf "%s\n" "${features[@]}"
-printf "%s\n" "${fixes[@]}"
-printf "%s\n" "${releases[@]}"
+#printf "%s\n" "${features[@]}"
+#printf "%s\n" "${fixes[@]}"
+#printf "%s\n" "${releases[@]}"
 
+if [ ${#features[@]} -eq 0 ]; then
+  echo ""
+else
+  echo '### Features' >> $OUTPUT_FILE
+  echo "${features[@]}" >> $OUTPUT_FILE
+fi
+
+if [ ${#fixes[@]} -eq 0 ]; then
+  echo ""
+else
+  echo '### Fixes' >> $OUTPUT_FILE
+  echo "${fixes[@]}" >> $OUTPUT_FILE
+fi
+
+if [ ${#deletions[@]} -eq 0 ]; then
+  echo ""
+else
+  echo '### Deletions' >> $OUTPUT_FILE
+  echo "${deletions[@]}" >> $OUTPUT_FILE
+fi
 
 #[[ $GIT_LOG = feat:* ]] && echo '### Features' >> $OUTPUT_FILE 
 #git log --pretty="- %s (%h)" $(git tag --sort=-taggerdate | head -2)...$(git tag --sort=-taggerdate | head -1) >> $OUTPUT_FILE
